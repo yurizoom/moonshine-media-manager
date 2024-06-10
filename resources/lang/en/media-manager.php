5ce6d70c16caf95c91bfb290048cd4d10a45211c
yurizoom <yurizoom@mail.ru>
2024-06-10 17:08:31 +0700

    Update media-manager.php
    Добавлены переводы ошибок на английский язык

--- resources/lang/en/media-manager.php
+++ resources/lang/en/media-manager.php
@@ -11,4 +11,9 @@ return [
     'url' => 'Url',
     'rename' => 'Rename & Move',
     'confirm_message' => 'Are you sure?'
+    'error' => [
+        'file_extension_not_allowed' => "File extension :ext is not allowed",
+        'only_local_storage' => "Media manager extension only works for local storage",
+        'file_not_exists' => "File or directory :path not exists",
+    ]
 ];
