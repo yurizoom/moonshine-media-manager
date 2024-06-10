7816ef4463bf4d671c5f354bec7232ca3e14e73e
yurizoom <yurizoom@mail.ru>
2024-06-10 17:09:15 +0700

    Update media-manager.php
    Диск по-умолчанию заменен на public

--- config/media-manager.php
+++ config/media-manager.php
@@ -2,7 +2,7 @@
 
 return [
     'auto_menu' => true,
-    'disk' => config('filesystem.default', 'local'),
+    'disk' => config('filesystem.default', 'public'),
     'allowed_ext' => 'jpg,jpeg,png,pdf,doc,docx,zip',
     'default_view' => 'table',
 ];
